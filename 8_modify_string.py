cc0e9329964c795e63b2967b28d9b16369931a24
Aritra <96793005+MeteoricMind@users.noreply.github.com>
2025-01-03 12:15:10 +0530

    Add files via upload

---
+++ 8_modify_string.py
@@ -0,0 +1,28 @@
+## UPPER FUNCTION
+
+a = "Hello, World!"
+print(a.upper())
+#----------------
+
+## Lower Case function 
+
+a = "Hello, World!"
+print(a.lower())
+#-------------------
+
+## strip function
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+#--------------------------
+
+## replace function
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+#--------------------------------
+
+## Split String function
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
