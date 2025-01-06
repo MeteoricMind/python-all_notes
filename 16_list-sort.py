868e4d6ce9cc7273086bff8179c62765ede9c5f6
Aritra <96793005+MeteoricMind@users.noreply.github.com>
2025-01-06 23:43:00 +0530

    Add files via upload

---
+++ 16_list-sort.py
@@ -0,0 +1,12 @@
+#sort function
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist)
+#-------------------------------------
+
+## reverse function
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+
