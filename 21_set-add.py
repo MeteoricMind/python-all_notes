775696ab5ae79fb9c98baf7d8a55fe2af5df6ec3
Aritra <96793005+MeteoricMind@users.noreply.github.com>
2025-01-11 11:14:42 +0530

    Add files via upload

---
+++ 21_set-add.py
@@ -0,0 +1,18 @@
+## add function
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+#-----------------------------------------
+
+## update function
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
+#-----------------------------------------
