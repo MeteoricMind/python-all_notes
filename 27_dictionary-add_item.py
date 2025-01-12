011c20156e754b635c7c950ed02731f83e18e977
Aritra <96793005+MeteoricMind@users.noreply.github.com>
2025-01-12 12:41:55 +0530

    Add files via upload

---
+++ 27_dictionary-add_item.py
@@ -0,0 +1,19 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+#-----------------------------
+
+## UPDATE FUNCTION
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"color": "red"})
+#------------------------------------
+
